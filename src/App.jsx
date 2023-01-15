7338747c24cebdf4f9b5b4b97b68f0ccdd43e599
Minh Quan Do <minhquan98vn@gmail.com>
2023-01-15 15:51:09 +0700

    ✨ feat: Complete project with maps addition

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,10 @@ const regexExp = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]
 const App = () => {
   const [input, setInput] = useState('');
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [geo, setGeo] = useState({
     ip: '',
-    location: '',
+    location: {},
     isp: ''
   });
 
@@ -29,13 +30,15 @@ const App = () => {
   }
 
   const handleSearch = async () => {
+    setLoading(true);
     try {
       let url;
       if (regexExp.test(input)) {
-        url = `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${import.meta.env.VITE_GEO_API_KEY}&ipAddress=${input}`
+        url = `https://geo.ipify.org/api/v2/country,city?apiKey=${import.meta.env.VITE_GEO_API_KEY}&ipAddress=${input}`
       } else {
         if (!isValidURL(input) && input.length > 0) {
           setError(true);
+          setLoading(false);
           return;
         }
         url = `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${import.meta.env.VITE_GEO_API_KEY}${input.length > 0 ? `&domain=${input}` : ''}`
@@ -44,6 +47,7 @@ const App = () => {
       setError(false);
       const res = await fetch(url);
       const { ip, location, isp } = await res.json();
+      setLoading(false);
       setGeo((prev) => ({ ...prev, ip, location, isp }));
     } catch (err) { console.log(err) }
   };
@@ -51,8 +55,10 @@ const App = () => {
   return (
     <div className='h-[100vh] w-full m-0 overflow-x-hidden relative'>
       <IpInformation geo={geo} error={error} input={input} setInput={setInput} handleSubmit={handleSubmit} />
-      <img src={patternBg} alt="pattern bg" className='md:w-full h-[280px] md:h-[250px] lg:h-[240px] absolute top-0 z-10 object-cover' />
-      <GoogleMaps geo={geo} />
+      <div className='h-full w-full overflow-hidden'>
+        <img src={patternBg} alt="pattern bg" className='md:w-full h-[280px] md:h-[250px] lg:h-[240px] absolute top-0 object-cover z-20' />
+        <GoogleMaps geo={geo} loading={loading} />
+      </div>
     </div>
   )
 }
--- src/components/googleMaps.jsx
+++ src/components/googleMaps.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { renderToStaticMarkup } from 'react-dom/server';
+import { MapContainer, Marker, TileLayer, Tooltip } from 'react-leaflet';
+import { divIcon } from 'leaflet';
+import "leaflet/dist/leaflet.css";
 
-const GoogleMaps = ({ geo }) => {
+import iconLocation from '../assets/icon-location.svg';
+
+const iconMarkup = renderToStaticMarkup(<img src={iconLocation} alt='icon location' className=' w-4 h-10' />);
+const customMarkerIcon = divIcon({
+  html: iconMarkup,
+});
+
+const GoogleMaps = ({ geo, loading }) => {
+  if (loading) return <></>
+  
+  const position = { lat: geo?.location.lat || 51.505, lng: geo?.location.lng || - 0.09 };
+
   return (
-    <div className='h-full w-full'></div>
+    <div className='absolute bottom-0 w-full h-[calc(100vh-280px)] md:h-[calc(100vh-250px)] lg:h-[calc(100vh-240px)] z-10'>
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position} icon={customMarkerIcon}>
+          <Tooltip direction='bottom' sticky>
+            <p className='text-center'>
+             <span className=' font-bold text-lg'>{ geo.isp }</span> <br /> ({`${geo.location?.city}, ${geo.location?.region} ${geo.location?.postalCode}`})
+            </p>
+          </Tooltip>
+        </Marker>
+      </MapContainer>
+    </div>
   )
 }
 
